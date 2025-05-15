c4fa3f2aee639ab73c58143281b0131d12d967d3
orbulant <vydranix@gmail.com>
2025-05-15 13:55:41 +0800

    Added a working modify top property but it's visually bugged

--- explorerItemHeight.js
+++ explorerItemHeight.js
@@ -1,9 +1,34 @@
-(function () {
-  Object.defineProperty(List.prototype, 'renderHeight', {
-    get() {
-      // Force the height to 36
-      return 36;
-    },
-    configurable: true,
+function modifyTopProperty(element) {
+  let id = element.id;
+  let index = parseInt(id.split('_').pop());
+  let expectedTopValue = index * 30;
+
+  let style = window.getComputedStyle(element);
+  let topValue = parseInt(style.top.replace('px', ''));
+
+  // Only change the top value if it's not already at the expected value
+  if (topValue !== expectedTopValue) {
+    element.style.top = `${expectedTopValue}px`;
+    element.style.height = `30px`;
+    element.style.lineHeight = `30px`;
+  }
+}
+
+let observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      mutation.addedNodes.forEach((node) => {
+        if (node.matches && node.matches('.explorer-folders-view .monaco-list .monaco-list-row')) {
+          modifyTopProperty(node);
+        }
+      });
+    }
+    if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+      if (mutation.target.matches('.explorer-folders-view .monaco-list .monaco-list-row')) {
+        modifyTopProperty(mutation.target);
+      }
+    }
   });
-})();
+});
+
+observer.observe(document.body, { childList: true, subtree: true, attributes: true, attributeFilter: ['style'] });
